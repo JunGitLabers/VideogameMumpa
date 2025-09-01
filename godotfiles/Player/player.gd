25907f39ba40f87ce7434fe09cae37199e928b32
Bedman478 <141074076+Bedman478@users.noreply.github.com>
2025-09-01 16:26:54 -0300

    Limitar movimiento de camara (Sin terminar)

--- godotfiles/Player/player.gd
+++ godotfiles/Player/player.gd
@@ -1,10 +1,21 @@
 extends CharacterBody3D
 const GlobalFunctions = preload("res://Assets/GlobalFunctions.gd");
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-const MOUSE_SENSITIVITY: float = 0.002
+@export var SPEED: float = 2.0
+@export var JUMP_VELOCITY: float = 4.5
+@export var MOUSE_SENSITIVITY: float = 0.0005
 var MOVEMENT_KIND = ""
+
+@onready var camera_3d: Camera3D = $CameraPivot/Camera3D
+var pitch: float =0.8 #tope vertical de la camara
+
+func _ready() -> void: 
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED 
+
+func _input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+
 
 func _get_key(event):
 	if Input.is_action_just_pressed(event):
